9ece2d3025c91a6763f36930d4d2dfc692e7f9be
Summer2906 <Summer.louise2906@gmail.com>
2025-11-20 15:10:43 +0000

    time slots

--- nook-web/app/basket/page.js
+++ nook-web/app/basket/page.js
@@ -253,13 +253,35 @@ export default function BasketPage() {
                 </div>
                 <div className="form-group">
                   <label htmlFor="delivery-time">Time *</label>
-                  <input
+                  <select
                     id="delivery-time"
-                    type="time"
                     value={deliveryTime}
                     onChange={(e) => setDeliveryTime(e.target.value)}
                     className="form-input"
-                  />
+                  >
+                    <option value="">Select a time</option>
+                    <option value="09:00">9:00 AM</option>
+                    <option value="09:30">9:30 AM</option>
+                    <option value="10:00">10:00 AM</option>
+                    <option value="10:30">10:30 AM</option>
+                    <option value="11:00">11:00 AM</option>
+                    <option value="11:30">11:30 AM</option>
+                    <option value="12:00">12:00 PM</option>
+                    <option value="12:30">12:30 PM</option>
+                    <option value="13:00">1:00 PM</option>
+                    <option value="13:30">1:30 PM</option>
+                    <option value="14:00">2:00 PM</option>
+                    <option value="14:30">2:30 PM</option>
+                    <option value="15:00">3:00 PM</option>
+                    <option value="15:30">3:30 PM</option>
+                    <option value="16:00">4:00 PM</option>
+                    <option value="16:30">4:30 PM</option>
+                    <option value="17:00">5:00 PM</option>
+                    <option value="17:30">5:30 PM</option>
+                    <option value="18:00">6:00 PM</option>
+                    <option value="18:30">6:30 PM</option>
+                    <option value="19:00">7:00 PM</option>
+                  </select>
                 </div>
               </div>
             </div>
